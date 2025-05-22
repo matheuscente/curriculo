dcce6143f0366355af31e1912077ed70f8836f4b
vicente <estagioti.extinpel@gmail.com>
2025-05-22 10:21:24 -0300

    add loading message onlogin

--- js/admin/pages/login.js
+++ js/admin/pages/login.js
@@ -1,29 +1,32 @@
 import Auth from "../auth/auth.js"
-
+import Errors from "../errors/errors.js";
 const auth = new Auth()
+const formatErrors = new Errors()
 
 const url = "https://cms-gkqy.onrender.com/api/v1/login";
 
     const form = document.querySelector("#login-form");
     const errorSpan = document.createElement('span')
+    const btn = document.querySelector('.login-item-button')
 
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
 
+        errorSpan.style.color = '#FF0000'
         errorSpan.innerText = 'Aguarde...'
         errorSpan.style.color = 'var(c--12)'
-        
-        form.insertBefore(errorSpan, form.lastChild)
+
+        form.insertBefore(errorSpan, btn)
         auth.login(url, form).then(() => {
             window.location.href = '/curriculo/admin/admin.html'
         }).catch((err) => {
              event.preventDefault();
-             if(err.response.data.errors === "invalid userName or password") {
+             if(err.response.data.errors === "invalid username or password") {
                 errorSpan.style.color = '#FF0000'
                 errorSpan.innerText = 'usuário ou senha invalidos'
              } else {
                 const errors = err.response.data.errors
-            formatErrors.returnErrors(errors)
+                formatErrors.returnErrors(errors)
              }
             
         })
