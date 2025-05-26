d412a0e6d55961fcafe1101064a1d199006bff05
vicente <estagioti.extinpel@gmail.com>
2025-05-26 11:13:55 -0300

    delete script.mjs

--- js/index/projects.mjs
+++
@@ -1,9 +0,0 @@
-import Exp from '../exp/exp.js'
-import Technologies from "../admin/templates/technologies.js";
-import Api from '../admin/api/api.js'
-
-const technologies = new Technologies()
-const api = new Api()
-const projects = new Exp(api, technologies)
-
-await projects.projectsIndex()
