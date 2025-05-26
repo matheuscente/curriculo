db588a9011613668ff68e54966a4a61e173596c7
vicente <estagioti.extinpel@gmail.com>
2025-05-26 11:55:35 -0300

    add close modal btn on index modal

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ import Exp from "../js/exp/exp.js";
 import Technologies from "../js/admin/templates/technologies.js";
 import Api from "../js/admin/api/api.js";
 import Modal from "../js/admin/modal/modal.js";
-import Areas from "../js/admin/templates/areas.js"
+import Areas from "../js/admin/templates/areas.js";
 
-const areas = new Areas()
+const areas = new Areas();
 const technologies = new Technologies();
 const api = new Api();
 const projects = new Exp(api, technologies);
@@ -16,40 +16,78 @@ const modal = new Modal({
 });
 
 const getAwaitMockTarget = {
-  id: "getAwait"
+  id: "getAwait",
 };
 
-await modal.openModal(getAwaitMockTarget, null, '.index-body');
+await modal.openModal(getAwaitMockTarget, null, ".index-body");
 
-projects.projectsIndex().then(() => {
+projects
+  .projectsIndex()
+  .then(() => {
+    const closeModalBtn = document.createElement("button");
+    closeModalBtn.classList.add("modal-project-close-btn");
+    closeModalBtn.innerText = "x";
+
+    const modalConfirmConteiner = document.querySelector(
+      ".modal-confirm-container"
+    );
+
+    modalConfirmConteiner.appendChild(closeModalBtn);
+
+    closeModalBtn.addEventListener("click", () => {
+      modal.closeModal();
+      const nav = document.querySelector(".nav-header"),
+        btnMobile = document.getElementById("btn-mobile"),
+        btnHeader = document.querySelectorAll(".btn-header-list");
+
+      btnMobile.addEventListener("click", (event) => {
+        toggleMenu(event, nav);
+      });
+      btnMobile.addEventListener("touchstart", (event) => {
+        toggleMenu(event, nav);
+      });
+
+      btnHeader.forEach((btn) => {
+        btn.addEventListener("touchstart", () => {
+          scrollBehavior();
+          nav.classList.remove("show");
+        });
+        btn.addEventListener("click", () => {
+          scrollBehavior();
+          nav.classList.remove("show");
+        });
+      });
+    });
+
     setTimeout(() => {
-        modal.closeModal();
+      modal.closeModal();
 
-  const nav = document.querySelector(".nav-header"),
-    btnMobile = document.getElementById("btn-mobile"),
-    btnHeader = document.querySelectorAll(".btn-header-list");
+      const nav = document.querySelector(".nav-header"),
+        btnMobile = document.getElementById("btn-mobile"),
+        btnHeader = document.querySelectorAll(".btn-header-list");
 
-  btnMobile.addEventListener("click", (event) => {
-    toggleMenu(event, nav);
-  });
-  btnMobile.addEventListener("touchstart", (event) => {
-    toggleMenu(event, nav);
-  });
+      btnMobile.addEventListener("click", (event) => {
+        toggleMenu(event, nav);
+      });
+      btnMobile.addEventListener("touchstart", (event) => {
+        toggleMenu(event, nav);
+      });
 
-  btnHeader.forEach((btn) => {
-    btn.addEventListener("touchstart", () => {
-      scrollBehavior();
-      nav.classList.remove("show");
-    });
-    btn.addEventListener("click", () => {
-      scrollBehavior();
-      nav.classList.remove("show");
-    });
-  });
-    }, 3000)
-}).catch(() => {
-    const modal = document.querySelector('.modal')
+      btnHeader.forEach((btn) => {
+        btn.addEventListener("touchstart", () => {
+          scrollBehavior();
+          nav.classList.remove("show");
+        });
+        btn.addEventListener("click", () => {
+          scrollBehavior();
+          nav.classList.remove("show");
+        });
+      });
+    }, 5000);
+  })
+  .catch(() => {
+    const modal = document.querySelector(".modal");
     modal.innerHTML = ` <p>
                             Ocorreu um erro ao buscar os projetos, por favor, recarregue a página e tente novamente.
-                        <p>`
-});
+                        <p>`;
+  });
